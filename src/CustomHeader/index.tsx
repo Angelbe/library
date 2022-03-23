5208e6d644a037dae79f22db9b0c17098892a4e0
Angel Bernardo <bernard.angel93@gmail.com>
2022-03-23 21:04:29 +0100

    Agnostic Update

---
+++ src/CustomHeader/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomHeader = () => {
+  return <div>Library</div>;
+};
+
+export default CustomHeader;
--- src/ZeeloHeader/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Image from "next/image";
-import leftCaret from "public/zeelo-logo.png";
-
-const ZeeloHeader = () => {
-  return <Image src={leftCaret} alt="caret Left" width={450} height={100} />;
-};
-
-export default ZeeloHeader;
